4cef797cb8d6dcaff3c40449ce291ec93f6c7dab
Nathan Martel <nathan.martel@etu.mines-ales.fr>
2025-04-04 20:22:12 +0200

    Redirect to homepage after successful registration #1

--- janus-website/janus-mdlw/janus-preauth.php
+++ janus-website/janus-mdlw/janus-preauth.php
@@ -4,7 +4,7 @@ include '/home/janus-storage/janus-db-connect/janus-db-connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $user = mysqli_real_escape_string($connexion, $_POST['username']);
-    $pass = mysqli_real_escape_string($connexion, $_POST['password']); // Pas de hachage
+    $pass = mysqli_real_escape_string($connexion, $_POST['password']); // Pensez à hacher le mot de passe
 
     // Requête préparée
     $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
@@ -15,8 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         mysqli_stmt_execute($stmt);
 
         if (mysqli_stmt_affected_rows($stmt) > 0) {
-            echo "Inscription réussie!";
-            # header("Location: ../accueil.php");
+            header("Location: ../janus-view/accueil.php");
             exit;
         } else {
             echo "Erreur : Aucune ligne insérée.";
@@ -30,3 +29,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 mysqli_close($connexion);
 ?>
+
---
+++ janus-website/janus-view/accueil.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo "Page d'accueil, donc connexion réussie";
+
+?>
